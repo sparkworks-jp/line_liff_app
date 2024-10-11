d607319359fdb007ff2ccf43fe4523f09b4eeb58
ro.gyou@harbest+1-at-010662016841 <ro.gyou@harbest.com>
2024-10-11 15:08:00 +0900

    auth

--- components/Layout.js
+++ components/Layout.js
@@ -7,7 +7,7 @@ import CartDrawer from './CartDrawer';
 import SimpleBottomNavigation from './Bottombutton';
 import Breadcrumb from './Breadcrumb';
  
-const Layout = ({ children }) => {
+const Layout = ({ children,userProfile,userId }) => {
   const { cart, setIsCartOpen } = useCart();
 
   return (
@@ -15,7 +15,14 @@ const Layout = ({ children }) => {
       <AppBar position="static">
         <Toolbar variant="dense">
           <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-            My Shop
+            My Shop          {userId && <p>欢迎，{userId}</p>}
+
+          </Typography>
+          <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+            {userId && <p>欢迎，{userId}</p>}
+          </Typography>
+          <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+          {userProfile && <p>欢迎，{userProfile.displayName}</p>}
           </Typography>
           <IconButton color="inherit" onClick={() => setIsCartOpen(true)}>
             <Badge badgeContent={cart.reduce((sum, item) => sum + item.quantity, 0)} color="secondary">
--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,3 @@
-
-
 import "../styles/globals.css";
 import { useState, useEffect } from "react";
 import liff from "@line/liff";
@@ -7,11 +5,55 @@ import Layout from "../components/Layout";
 import { CartProvider } from '../components/CartContext';
 
 function MyApp({ Component, pageProps }) {
+  const [liffObject, setLiffObject] = useState(null);
+  const [liffError, setLiffError] = useState(null);
+  const [userProfile, setUserProfile] = useState(null);
+  const [userId, setUserId] = useState(null);
+  const [userName, setUserName] = useState(null);
+
+
+  // Execute liff.init() when the app is initialized
+  useEffect(() => {
+    console.log("start liff.init()...");
+    liff
+      .init({ liffId: process.env.NEXT_PUBLIC_LIFF_ID})
+      .then(() => {    
+        console.log("liff.init() done");
+        setLiffObject(liff);
+        if (liff.isLoggedIn()) {
+          liff.getProfile().then(profile => {
+            const userId = profile.userId;
+            const displayName = profile.displayName;
+            setUserId(userId);
+            setUserName(displayName);
+            setUserProfile(profile)
+
+          }).catch(err => console.error('Error getting profile:', err));
+        } else {
+          liff.login();
+        }
+
+      })
+      .catch((error) => {
+        console.log(`liff.init() failed: ${error}`);
+        if (!process.env.liffId) {
+          console.info(
+            "LIFF Starter: Please make sure that you provided `LIFF_ID` as an environmental variable."
+          );
+        }
+        setLiffError(error.toString());
+      });
+  }, []);
+
+console.log("user id",userId)
+console.log("user name",userName)
+console.log("user profile",userProfile)
+
 
   return (
     
       <CartProvider>
-        <Layout>
+        <Layout userProfile={userProfile} userId={userId}>
         <Component {...pageProps} />
         </Layout>
       </CartProvider>
