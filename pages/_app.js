0c2f41f368f413ff41480af7e29d7de8debfc829
ro.gyou@harbest+1-at-010662016841 <ro.gyou@harbest.com>
2024-10-11 17:59:24 +0900

    liff.init

--- pages/_app.js
+++ pages/_app.js
@@ -13,41 +13,41 @@ function MyApp({ Component, pageProps }) {
 
 
   // Execute liff.init() when the app is initialized
-//   useEffect(() => {
-//     console.log("start liff.init()...");
-//     liff
-//       .init({ liffId: process.env.NEXT_PUBLIC_LIFF_ID})
-//       .then(() => {    
-//         console.log("liff.init() done");
-//         setLiffObject(liff);
-//         if (liff.isLoggedIn()) {
-//           liff.getProfile().then(profile => {
-//             const userId = profile.userId;
-//             const displayName = profile.displayName;
-//             setUserId(userId);
-//             setUserName(displayName);
-//             setUserProfile(profile)
+  useEffect(() => {
+    console.log("start liff.init()...");
+    liff
+      .init({ liffId: process.env.NEXT_PUBLIC_LIFF_ID})
+      .then(() => {    
+        console.log("liff.init() done");
+        setLiffObject(liff);
+        if (liff.isLoggedIn()) {
+          liff.getProfile().then(profile => {
+            const userId = profile.userId;
+            const displayName = profile.displayName;
+            setUserId(userId);
+            setUserName(displayName);
+            setUserProfile(profile)
 
-//           }).catch(err => console.error('Error getting profile:', err));
-//         } else {
-//           liff.login();
-//         }
+          }).catch(err => console.error('Error getting profile:', err));
+        } else {
+          liff.login();
+        }
 
-//       })
-//       .catch((error) => {
-//         console.log(`liff.init() failed: ${error}`);
-//         if (!process.env.liffId) {
-//           console.info(
-//             "LIFF Starter: Please make sure that you provided `LIFF_ID` as an environmental variable."
-//           );
-//         }
-//         setLiffError(error.toString());
-//       });
-//   }, []);
+      })
+      .catch((error) => {
+        console.log(`liff.init() failed: ${error}`);
+        if (!process.env.liffId) {
+          console.info(
+            "LIFF Starter: Please make sure that you provided `LIFF_ID` as an environmental variable."
+          );
+        }
+        setLiffError(error.toString());
+      });
+  }, []);
 
-// console.log("user id",userId)
-// console.log("user name",userName)
-// console.log("user profile",userProfile)
+console.log("user id",userId)
+console.log("user name",userName)
+console.log("user profile",userProfile)
 
 
   return (
