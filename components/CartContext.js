07847537ac2d19fe496bcca01e1f4fc645b2d883
ro.gyou@harbest+1-at-010662016841 <ro.gyou@harbest.com>
2024-10-15 11:43:16 +0900

    cart bug3

--- components/CartContext.js
+++ components/CartContext.js
@@ -2,14 +2,56 @@ import React, { createContext, useState, useContext } from 'react';
 
 export const CartContext = createContext();
 export const useCart = () => useContext(CartContext);
+
+
+const CART_KEY = 'cart';
+const CART_EXPIRATION_KEY = 'cartExpiration';
+// 現在のタイムスタンプを取得する関数
+const getCurrentTimestamp = () => new Date().getTime();
+
+// カートの有効期限が切れているかをチェックする関数
+const isCartExpired = () => {
+  const expiration = localStorage.getItem(CART_EXPIRATION_KEY);
+  if (!expiration) return true;
+  return getCurrentTimestamp() > parseInt(expiration, 10);
+};
+
+// カートを保存し、有効期限を設定する関数
+const saveCartToLocalStorage = (cart) => {
+  localStorage.setItem(CART_KEY, JSON.stringify(cart));
+  localStorage.setItem(CART_EXPIRATION_KEY, getCurrentTimestamp() + process.env.NEXT_PUBLIC_ORDER_EXPIRATION_TIME);
+};
+
+// localStorage からカートを読み込む関数（期限切れの場合は削除）
+const loadCartFromLocalStorage = () => {
+  if (isCartExpired()) {
+    localStorage.removeItem(CART_KEY);
+    localStorage.removeItem(CART_EXPIRATION_KEY);
+    return [];
+  }
+  const savedCart = localStorage.getItem(CART_KEY);
+  return savedCart ? JSON.parse(savedCart) : [];
+};
+
 export const CartProvider = ({ children }) => {
   const [cart, setCart] = useState([]);
   const [isCartOpen, setIsCartOpen] = useState(false);
+  // 初期化時に localStorage からカートを読み込む
+    useEffect(() => {
+      const initialCart = loadCartFromLocalStorage();
+      setCart(initialCart);
+    }, []);
+    // カートが更新されたとき、localStorage に保存する
+    useEffect(() => {
+      if (cart.length > 0) {
+        saveCartToLocalStorage(cart);
+      }
+    }, [cart]);
 
   const addToCart = (product) => {
-    console.log('Adding to cart:', product); // 添加的商品信息
+    console.log('Adding to cart:', product); 
     setCart(prevCart => {
-      console.log('Previous cart state:', prevCart);  // 之前购物车的状态
+      console.log('Previous cart state:', prevCart);  
 
       const existingItemIndex = prevCart.findIndex(item => item.id === product.id);
       if (existingItemIndex !== -1) {
@@ -22,7 +64,7 @@ export const CartProvider = ({ children }) => {
           quantity: newCart[existingItemIndex].quantity + product.quantity, 
           flavorOptions: product.flavorOptions
         };
-        console.log('Updated cart:', newCart);  // 打印更新后的购物车
+        console.log('Updated cart:', newCart);  
 
         return newCart;
       } else {
