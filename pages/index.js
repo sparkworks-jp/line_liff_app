aedd4d25005c71a4ae3066549d376ea45e739925
ro.gyou@harbest+1-at-010662016841 <ro.gyou@harbest.com>
2024-10-17 15:27:52 +0900

    update

---
+++ pages/index.js
@@ -0,0 +1,23 @@
+
+import Head from "next/head";
+import Link from 'next/link';
+import styles from '../styles/Home.module.css';
+
+export default function Home({ userProfile }) {
+  return (
+    <div className={styles.container}>X
+      <Head>
+        <title>Coffee Shop</title>
+      </Head>
+      <div className={styles.home}>
+        <h1 className={styles.title}>スパークタイムズカフェへようこそ</h1>
+        {userProfile?.displayName && (
+          <p style={{ color: 'white' }}>Hello, {userProfile.displayName}!</p>
+        )}
+        <Link href="/shop">
+          <a className={styles.button}>注文開始</a>
+        </Link>
+      </div>
+    </div>
+  );
+}
