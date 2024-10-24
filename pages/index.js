ab25d77ea15a96a639225d99e09c4b72b05f178f
ro.gyou@harbest+1-at-010662016841 <ro.gyou@harbest.com>
2024-10-24 16:36:00 +0900

    Home　delete

--- pages/index.js
+++ pages/index.js
@@ -1,23 +1,14 @@
 
-import Head from "next/head";
-import Link from 'next/link';
-import styles from '../styles/Home.module.css';
 
-export default function Home({ userProfile }) {
-  return (
-    <div className={styles.container}>X
-      <Head>
-        <title>Coffee Shop</title>
-      </Head>
-      <div className={styles.home}>
-        <h1 className={styles.title}>スパークタイムズカフェへようこそ</h1>
-        {userProfile?.displayName && (
-          <p style={{ color: 'white' }}>Hello, {userProfile.displayName}!</p>
-        )}
-        <Link href="/shop">
-          <a className={styles.button}>注文開始</a>
-        </Link>
-      </div>
-    </div>
-  );
+import { useEffect } from 'react';
+import { useRouter } from 'next/router';
+
+export default function Home() {
+  const router = useRouter();
+
+  useEffect(() => {
+    router.push('/shop');
+  }, []);
+
+  return null; 
 }
