0416911ff69405c33d93c7c59cd5c26ae9f38525
ro.gyou@harbest+1-at-010662016841 <ro.gyou@harbest.com>
2024-11-13 18:30:08 +0900

    line login

--- pages/_app.js
+++ pages/_app.js
@@ -13,71 +13,6 @@ function MyApp({ Component, pageProps }) {
   const [userName, setUserName] = useState(null);
 
   // Execute liff.init() when the app is initialized
-  // useEffect(() => {
-  //   // 環境変数のチェック
-  //   if (!process.env.NEXT_PUBLIC_LIFF_ID) {
-  //     console.error('LIFF IDが設定されていません。.env.localファイルを確認してください。');
-  //     setLiffError('LIFF IDが設定されていません');
-  //     return;
-  //   }
-
-  //   console.log("LIFF初期化中...");
-  //   console.log("LIFF ID:", process.env.NEXT_PUBLIC_LIFF_ID);
-
-  //   liff
-  //     .init({
-  //       liffId: process.env.NEXT_PUBLIC_LIFF_ID,
-  //       withLoginOnExternalBrowser: true, // 外部ブラウザでもログインを有効にする
-  //     })
-  //     .then(() => {
-  //       console.log("LIFF初期化が完了しました");
-  //       setLiffObject(liff);
-        
-  //       // ログイン状態の確認
-  //       if (liff.isLoggedIn()) {
-  //         return liff.getProfile();
-  //       } else {
-  //         console.log("ユーザーが未ログインです。ログインページへリダイレクトします...");
-  //         liff.login();
-  //       }
-  //     })
-  //     .then((profile) => {
-  //       if (profile) {
-  //         console.log("ユーザー情報を取得しました:", profile);
-  //         setUserProfile(profile);
-  //         setUserId(profile.userId);
-  //         setUserName(profile.displayName);
-          
-  //         // ユーザー情報をローカルストレージに保存
-  //         localStorage.setItem("user", JSON.stringify(profile));
-          
-  //         // トークン情報の取得とログ
-  //         const accessToken = liff.getAccessToken();
-  //         const idToken = liff.getIDToken();
-  //         const idToken2 = liff.getIdToken();
-  //         const decodedIdToken = liff.getDecodedIDToken();
-  //         const decodedIdToken2 = liff.getDecodedIdToken();
-
-  //         console.log("アクセストークン:", accessToken);
-  //         console.log("idToken:", idToken);
-  //         console.log("idToken2:", idToken2);
-  //         console.log("decodedIdToken:", decodedIdToken);
-  //         console.log("decodedIdToken2:", decodedIdToken2);
-
-  //       }
-  //     })
-  //     .catch((err) => {
-  //       console.error("LIFF初期化に失敗しました:", err);
-  //       setLiffError(err.message);
-  //       if (err.code) {
-  //         console.error(`エラーコード: ${err.code}`);
-  //       }
-  //       if (err.message) {
-  //         console.error(`エラーメッセージ: ${err.message}`);
-  //       }
-  //     });
-  // }, []);
-
   const getTokenInfo = async () => {
     const tokenInfo = {};
 
@@ -90,7 +25,6 @@ function MyApp({ Component, pageProps }) {
       console.log("getAccessToken エラー:", error.message);
     }
 
-    // 测试 getIDToken
     try {
       const idToken = await liff.getIDToken();
       console.log("IDトークン (getIDToken):", idToken);
@@ -99,16 +33,8 @@ function MyApp({ Component, pageProps }) {
       console.log("getIDToken エラー:", error.message);
     }
 
-    // 测试 getIdToken
-    try {
-      const idToken2 = await liff.getIdToken();
-      console.log("IDトークン (getIdToken):", idToken2);
-      tokenInfo.idToken2 = idToken2;
-    } catch (error) {
-      console.log("getIdToken エラー:", error.message);
-    }
 
-    // 测试 getDecodedIDToken
+
     try {
       const decodedIdToken = await liff.getDecodedIDToken();
       console.log("デコードされたIDトークン (getDecodedIDToken):", decodedIdToken);
@@ -117,16 +43,8 @@ function MyApp({ Component, pageProps }) {
       console.log("getDecodedIDToken エラー:", error.message);
     }
 
-    // 测试 getDecodedIdToken
-    try {
-      const decodedIdToken2 = await liff.getDecodedIdToken();
-      console.log("デコードされたIDトークン (getDecodedIdToken):", decodedIdToken2);
-      tokenInfo.decodedIdToken2 = decodedIdToken2;
-    } catch (error) {
-      console.log("getDecodedIdToken エラー:", error.message);
-    }
 
-    // 打印所有成功获取的 token 信息
+
     console.log("取得できたすべてのトークン情報:", tokenInfo);
     return tokenInfo;
   };
