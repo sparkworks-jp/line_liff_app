9ab60a6c600b91dc30a4fbd0857a49f4b67a0cb8
ro.gyou@harbest+1-at-010662016841 <ro.gyou@harbest.com>
2024-11-13 17:50:45 +0900

    bug

--- pages/cart.js
+++ pages/cart.js
@@ -1,5 +1,5 @@
 import { useContext } from 'react';
-import { CartContext } from '../components/CartContext';
+import { CartContext } from '../context/CartContext';
 import CartItem from '../components/CartItem';
 import Link from 'next/link';
  
