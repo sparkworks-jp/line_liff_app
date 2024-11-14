ecd0db1a5550b8e1242ea54c59cc2ab2a9492714
ro.gyou@harbest+1-at-010662016841 <ro.gyou@harbest.com>
2024-11-14 14:51:19 +0900

    bug

--- context/AuthContext.js
+++ context/AuthContext.js
@@ -1,4 +1,4 @@
-import  {  useState } from 'react';
+import  {  useState, useEffect } from 'react';
 export function AuthProvider({ children }) {
   const [userProfile, setUserProfile] = useState(null);
   const [idToken, setIdToken] = useState(null);
